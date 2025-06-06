fc8f2095276e0edee454c9372ed4a40e6e6a85f0
Fadelmystic <Mohamedfadelbeniche@gmail.com>
2025-06-06 13:06:13 +0100

    Delete listing-service directory

--- listing-service/src/main/java/com/foodsurplus/listing_service/infrastructure/api/dto/VerifierDisponibiliteRequest.java
+++
@@ -1,8 +0,0 @@
-package com.foodsurplus.listing_service.infrastructure.api.dto;
-
-public class VerifierDisponibiliteRequest {
-    private int quantiteDemandee;
-
-    public int getQuantiteDemandee() { return quantiteDemandee; }
-    public void setQuantiteDemandee(int quantiteDemandee) { this.quantiteDemandee = quantiteDemandee; }
-} 
