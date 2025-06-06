25b4d02e29eeb1093c4fa4061e11b811b0818191
Fadelmystic <Mohamedfadelbeniche@gmail.com>
2025-06-06 13:05:30 +0100

    Delete listing-service directory

--- listing-service/src/main/java/com/foodsurplus/listing_service/infrastructure/api/dto/MettreAJourQuantiteRequest.java
+++
@@ -1,8 +0,0 @@
-package com.foodsurplus.listing_service.infrastructure.api.dto;
-
-public class MettreAJourQuantiteRequest {
-    private int quantiteCommandee;
-
-    public int getQuantiteCommandee() { return quantiteCommandee; }
-    public void setQuantiteCommandee(int quantiteCommandee) { this.quantiteCommandee = quantiteCommandee; }
-} 
